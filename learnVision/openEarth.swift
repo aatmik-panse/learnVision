df40f5cd2ce1041b78bdcddf5d52a264eec49dd1
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-02-27 00:43:17 +0530

    earth added

---
+++ learnVision/openEarth.swift
@@ -0,0 +1,18 @@
+//
+//  openEarth.swift
+//  learnVision
+//
+//  Created by Aatmik Panse on 26/02/24.
+//
+
+import SwiftUI
+
+struct openEarth: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    openEarth()
+}
